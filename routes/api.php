d03a005687599cf79590b64cccf61d7a76e048b1
Lenielyn <lenielyndponteras1234@gmail.com>
2025-02-18 17:20:15 +0800

    Commit

--- routes/api.php
+++ routes/api.php
@@ -24,13 +24,5 @@ Route::get('/users', function (Request $request) {
     // ...
 });
 
-Route::view('/welcome', 'welcome');
- 
-Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
-
-Route::get($uri, $callback);
-Route::post($uri, $callback);
-Route::put($uri, $callback);
-Route::patch($uri, $callback);
-Route::delete($uri, $callback);
-Route::options($uri, $callback);
+Route::get('/register', [UserController::class, 'register']);
+Route::get('/login', [UserController::class, 'login']);
